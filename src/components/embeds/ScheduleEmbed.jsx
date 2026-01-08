aba5302a0089e3074b7ed223902cf0f2443eef73
Jeep <jeep@jeepmeta.xyz>
2026-01-07 20:21:10 -0500

    Switch to HashRouter for GitHub Pages

---
+++ src/components/embeds/ScheduleEmbed.jsx
@@ -0,0 +1,15 @@
+export default function ScheduleEmbed() {
+  return (
+    <iframe
+      src="https://jeepmeta.github.io/789StudiosScheduleWidget/"
+      title="789 Studios Schedule"
+      loading="lazy"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+        overflow: "hidden",
+      }}
+    />
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { RootConfigProvider } from "./context/RootConfigContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <RootConfigProvider>
       <App />
     </RootConfigProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
